e880ae2619f3cbba3db20eeb60920ca3c37985a8
vNeon <vnio666@aucklanduni.ac.nz>
2015-09-07 17:38:32 +1200

    Now checks for invalid filenames, and if file exists

--- src/mainview/SaveSpeechFrame.java
+++ src/mainview/SaveSpeechFrame.java
@@ -9,15 +9,17 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.File;
 
 public class SaveSpeechFrame extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
 	private JTextField textField_1;
+	private String message;
 	
 	private JFrame thisFrame = this;
 	/**
@@ -41,39 +43,57 @@ public class SaveSpeechFrame extends JFrame {
 	 */
 	public SaveSpeechFrame() {
 		setTitle("Save Speech");
-		setBounds(100, 100, 600, 400);
+		setBounds(100, 100, 550, 251);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(12, 102, 576, 25);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
 		textField_1 = new JTextField();
-		textField_1.setBounds(116, 173, 472, 25);
+		textField_1.setBounds(116, 82, 298, 25);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
-		JLabel lblCurrentSpeech = new JLabel("Current Speech:");
-		lblCurrentSpeech.setBounds(12, 65, 206, 25);
-		contentPane.add(lblCurrentSpeech);
-		
 		JLabel lblNewLabel = new JLabel("Name of file:");
-		lblNewLabel.setBounds(12, 175, 104, 25);
+		lblNewLabel.setBounds(12, 82, 104, 25);
 		contentPane.add(lblNewLabel);
 		
 		JButton btnNewButton = new JButton("Save");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				SaveSpeech ss = new SaveSpeech(textField.getText(), textField_1.getText());
+				// the files name
+				String fileName = textField_1.getText();
+				String pattern = "^[a-zA-Z0-9_]*$";
+				
+				//checks if file name is valid
+				if (!fileName.matches(pattern)){
+					MessageFrame mf = new MessageFrame("Error", "Error:", "Invalid Name!");
+					mf.setVisible(true);
+					return;
+				}else if (fileName.equals("")){
+					MessageFrame mf = new MessageFrame("Error", "Error:", "No Name!");
+					mf.setVisible(true);
+					return;
+				}
+				
+				// used to create the file
+				fileName = fileName + ".wav";
+				File tmpFile = new File(fileName);
+				
+				// checks if file already exists
+				if (tmpFile.exists() && !tmpFile.isDirectory()){
+					MessageFrame mf = new MessageFrame("Error", "Error:", "File Already Exists!");
+					mf.setVisible(true);
+					return;
+				}
+				
+				// Creates the wave file the user requests
+				SaveSpeech ss = new SaveSpeech(message, fileName);
 				ss.execute();
 				thisFrame.dispose();
 			}
 		});
-		btnNewButton.setBounds(46, 269, 117, 25);
+		btnNewButton.setBounds(50, 159, 117, 25);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Cancel");
@@ -82,15 +102,19 @@ public class SaveSpeechFrame extends JFrame {
 				thisFrame.dispose();
 			}
 		});
-		btnNewButton_1.setBounds(428, 269, 117, 25);
+		btnNewButton_1.setBounds(379, 159, 117, 25);
 		contentPane.add(btnNewButton_1);
 		
 		JLabel lblNewLabel_1 = new JLabel("Save Speech");
 		lblNewLabel_1.setFont(new Font("Dialog", Font.BOLD, 25));
 		lblNewLabel_1.setBounds(12, 12, 313, 33);
 		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblwav = new JLabel(".wav");
+		lblwav.setBounds(426, 82, 70, 25);
+		contentPane.add(lblwav);
 	}
 	public void setSpeech(String message){
-		textField.setText(message);
+		this.message = message;
 	}
 }
