a2ee75949fb2046233ee48090403164131743d28
vNeon <vnio666@aucklanduni.ac.nz>
2015-09-19 00:13:58 +1200

    Allows user to select a video file. Fixed issue where output file must have .avi extention.

--- src/mainview/AddMp3File.java
+++ src/mainview/AddMp3File.java
@@ -1,10 +1,10 @@
 package mainview;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
-
 
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 
@@ -18,6 +18,7 @@ public class AddMp3File extends SwingWorker<Object,Integer> {
 	private EmbeddedMediaPlayer video;
 	private JLabel statuslbl;
 	private boolean playVideo;
+	private File outputName;
 	
 	/**
 	 * constructor
@@ -28,6 +29,7 @@ public class AddMp3File extends SwingWorker<Object,Integer> {
 		this.fileName = fileName;
 		this.vidFile = vidFile;
 		this.outputFile = outputFile;
+		outputName = new File(outputFile);
 		this.video = video;
 		this.statuslbl= statuslbl;
 		this.playVideo = playVideo;
@@ -51,18 +53,15 @@ public class AddMp3File extends SwingWorker<Object,Integer> {
 	}
 	@Override 
 	protected void process(List<Integer> chunks){
-		this.statuslbl.setText("Creating "+outputFile+", please wait...");
+		this.statuslbl.setText("Creating "+outputName.getName()+", please wait...");
 	}
 	@Override
 	protected void done(){
-		this.statuslbl.setText("Successfully created "+outputFile+"!");
-		video.playMedia(outputFile);
+		this.statuslbl.setText("Successfully created "+outputName.getName()+"!");
 		
 		if(playVideo){
+			video.playMedia(outputFile);
 			video.start();
-		} else{
-			video.start();
-			video.pause();
 		}
 	}
 
